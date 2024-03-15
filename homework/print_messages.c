06b38f634f791a92925a92e6349953c2ffa872cb
Ubuntu <andreasavastano4388@gmail.com>
2024-03-15 15:35:47 -0400

    qualche modifica

--- homework/print_messages.c
+++ homework/print_messages.c
@@ -14,16 +14,17 @@ int main(int argc, char *argv[]){
         
         if(pid<0){
             fprintf(stderr,"fork failed");
-            exit(0);
+            exit(1);
         }else if(pid==0){
             sleep(rand()%10);
             
             printf("PID: %d, Messaggio: %s\n",getpid(),argv[i]);
             printf("Mio padre ha PID:%d\n",getppid());
-            exit(1);
+            exit(0);
         }else{
             printf("Sono il padre e pid:%d\n",pid);
             wait(NULL);
         }        
     }
+    return 0;
 }
