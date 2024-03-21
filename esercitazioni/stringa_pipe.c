6a37cb437e19e555d42335e22f4390517ca92795
Ubuntu <andreasavastano4388@gmail.com>
2024-03-21 14:43:16 -0400

    da modificare

--- esercitazioni/stringa_pipe.c
+++ esercitazioni/stringa_pipe.c
@@ -17,41 +17,44 @@ int main(){
     int i;
     int fd[2];
     int verifica=pipe(fd);
-        if(verifica==-1){
-            fprintf(stderr,"Non è stata creata la pipe anonima!\n");
-            return 1;
-        }
+    if(verifica==-1){
+        fprintf(stderr,"Non è stata creata la pipe anonima!\n");
+        return 1;
+    }
     
     Ts xyz;    
     
-    for(i=0;i<N;i++){
+    pid_t pid=fork();
+    if(pid<0){
+        fprintf(stderr,"errore nella fork!\n");
+        return 1;
+    }else if(pid==0){
+        close(fd[1]);
         
-    
-        pid_t pid=fork();
-        if(pid<0){
-            fprintf(stderr,"errore nella fork!\n");
-              return 1;
-        }else if(pid==0){
-            close(fd[1]);
-            int ricevuti=read(fd[1],&xyz,sizeof(xyz));
-            
-            printf("\nStampo la struttura dal figlio: x:%.2f y:%.2f z:%.2f\n",xyz.x,xyz.y,xyz.z);
-            
-            close(fd[0]);
-            return 0;
-        }else{
-            close(fd[0]);
+        for(i=0;i<N;i++){        
+            int ricevuti=read(fd[0],&xyz,sizeof(xyz));  
+            if(ricevuti<sizeof(xyz)){
+                
+            }s
+            printf("\nStampo la struttura dal figlio: x:%.2f y:%.2f z:%.2f\n",xyz.x,xyz.y,xyz.z);        
+        }        
+        close(fd[0]);
+    }else{
+        close(fd[0]);
+        
+        for(i=0;i<N;i++){        
             xyz.x=i;
             xyz.y=i*2;
             xyz.z=3*i/2;
-            printf("\nStampo la struttura dal padre: x:%.2f y:%.2f z:%.2f\n",xyz.x,xyz.y,xyz.z);
             int inviati=write(fd[1],&xyz,sizeof(xyz));
             if(inviati<sizeof(xyz)){
                 printf("Errore");
                 return 1;
             }
-            close(fd[1]);
-            return 0;
+            printf("\nStampo la struttura dal figlio: x:%.2f y:%.2f z:%.2f\n",xyz.x,xyz.y,xyz.z);        
         }
+        
+        close(fd[1]);
+        return 0;
     }
 }
