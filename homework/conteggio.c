1ce43a63b344b210eaec3c9c661cc8c5e9829702
Ubuntu <andreasavastano4388@gmail.com>
2024-04-11 05:47:00 -0400

    ecco

--- homework/conteggio.c
+++ homework/conteggio.c
@@ -24,25 +24,18 @@ int main(int argc, char*argv[]){
         fprintf(stderr, "Inserire da riga di comando un numero >= 0\n");
         return EXIT_FAILURE;
     }    
-    int fd[2];
+    int fd[2],mess;
     if(pipe(fd)==-1){
         fprintf(stderr,"Errore nella creazione della pipe\n");
         return 1;
     }    
     pid_t pid=fork();
-    int mess;
     
     if(pid<0){
         fprintf(stderr,"Errore nella creazione del processo figlio\n");
         return 1;
     }else if(pid==0){
         close(fd[0]);
-        
-        /*for(int i=0; i<=x; i++){
-            v[i]=x-i;
-            printf("(figlio) i=%d -> %d\n",i,v[i]);
-        }
-        write(fd[1], v, sizeof(v));*/
         
         for(int i=0; i<=x; i++){
             mess=x-i;
@@ -55,11 +48,6 @@ int main(int argc, char*argv[]){
         close(fd[1]);
         
         int ricevuti;
-        
-        /*read(fd[0], v, sizeof(v));
-        for(int i=0; i<=x; i++){
-            printf("(padre) i=%d -> %d\n",i,v[i]);
-        }*/
         
         printf("padre: ");
         for(int i=0; i<=x; i++){
