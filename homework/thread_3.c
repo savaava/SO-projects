cb892a70cdd175aa3ae3cbfbba56b060a0137d1d
Ubuntu <andreasavastano4388@gmail.com>
2024-05-01 16:32:56 -0400

    thread_3

--- homework/thread_3.c
+++ homework/thread_3.c
@@ -1,39 +1,44 @@
-/*Esercizio 3
-Scrivere in C un programma che prenda come parametro sulla linea di comando un numero intero n e
-crea n threads. Ciascun thread stampa il proprio indice. L’indice del thread è così determinato: il primo thread creato ha indice 0, il secondo 1, ..., l’n-esimo ha indice n-1.*/
+/*Scrivere in C un programma che prenda come parametro sulla linea di comando un numero intero n e
+crea n threads. Ciascun thread stampa il proprio indice. L’indice del thread è così determinato: il primo
+thread creato ha indice 0, il secondo 1, ..., l’n-esimo ha indice n-1.*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 
-void*lettura(void*arg);
+void* thread_stampa_index(void* index);
 
 int main(int argc, char*argv[]){
     if(argc!=2){
-        fprintf(stderr,"Non sono stati passati due stringhe da riga di comando!");
+        fprintf(stderr,"Inserire un numero n da riga di comando\n");
         return 1;
     }
     
     int n=atoi(argv[1]);
-    int i,arg[n];
-    pthread_t tids[n];
-    
-    for(i=0; i<n; i++){
-        arg[i]=i;
-        if(pthread_create(tids+i,NULL,lettura,arg+i) !=0){
-            fprintf(stderr,"errore nella creazione del thread figlio!");
-            return 2;
-        }
-        printf("Sono qui\n");
+    if(n<0){
+        fprintf(stderr,"Inserire un numero n>=0 da riga di comando\n");
+        return 1;
     }
-    for(i=0; i<n; i++)
-        pthread_join(tids[i],NULL);
     
-    return 0;
+    pthread_t tid[n];
+    int tmp[n];
+    
+    for(int i=0; i<n; i++){
+        tmp[i]=i;
+        if(pthread_create(tid+i, NULL, thread_stampa_index, tmp+i)!=0){
+            fprintf(stderr,"errore nella creazione del thread\n");
+            return 1;
+        }
+    }
+    
+    for(int i=0; i<n; i++){
+        pthread_join(tid[i],NULL);
+    }
+    
 }
 
-void*lettura(void*arg){
-    int indice_curr=*((int*)arg);
-    printf("L'indice del thread corrente è: %d\n",indice_curr);
+void* thread_stampa_index(void* index){
+    printf("Sono il thread con indice: %d \n",*((int*)index));
+    
     pthread_exit(NULL);
 }
